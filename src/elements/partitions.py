2a669368f930aa9e1f6142ab8a7cd27c488a40db
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-04-24 21:44:05 +0100

    Add src/elements/partitions.py

---
+++ src/elements/partitions.py
@@ -0,0 +1,28 @@
+"""Module partitions.py"""
+import typing
+
+
+class Partitions(typing.NamedTuple):
+    """
+    The data type class ⇾ Partitions<br><br>
+
+    Attributes<br>
+    ----------<br>
+    <b>ts_id</b>: int<br>
+        The identification code of a time series.<br><br>
+    <b>catchment_id</b>: int<br>
+        The identification code of a catchment area.<br><br>
+    <b>gauge_datum</b>: float<br>
+        References:
+            <a href="https://timeseriesdoc.sepa.org.uk/api-documentation/before-you-start/what-data-are-available/#Level"
+              target="_blank">River Level (Stage)</a>,
+            <a href="https://www.sepa.org.uk/environment/water/water-levels/"  target="_blank">Water Levels</a>,
+            <a href="https://waterdata.usgs.gov/blog/gage_height/" target="_blank">United States Geological Survey</a><br><br>
+    <b>datestr</b>: str<br>
+        The <b>date string of the start date of a period</b>, format %Y-%m-%d,  i.e., YYYY-mm-dd.<br><br>
+    """
+
+    ts_id: int
+    catchment_id: int
+    gauge_datum: float
+    datestr: str
