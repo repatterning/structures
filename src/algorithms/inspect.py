efb779836fa6d6da49a1edddaaa1f002f5eb3c5e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-04-24 23:56:39 +0100

    Update src/algorithms/inspect.py

--- src/algorithms/inspect.py
+++ src/algorithms/inspect.py
@@ -2,6 +2,7 @@ import datetime
 import logging
 
 import pandas as pd
+import src.elements.partitions as pr
 
 
 class Inspect:
@@ -10,25 +11,32 @@ class Inspect:
         pass
 
     @staticmethod
-    def __get_reference(_maximum, _minimum) -> pd.DataFrame:
+    def __get_reference(_maximum, _minimum):
+
+        logging.info('_maximum: %s', type(_maximum))
 
         latest = datetime.datetime.now().year
 
         dates = pd.date_range(
             start=_minimum, end=_maximum, freq='0.25h', inclusive='left' if _maximum.year != latest else 'both')
-        logging.info('Inferred Frequency: %s', dates.inferred_freq)
 
-        return pd.DataFrame(data={'date': dates})
+        return dates
 
-    def exc(self, data: pd.DataFrame):
+    def exc(self, data: pd.DataFrame, partition: pr.Partitions):
         """
 
         :param data:
+        :param partition:
         :return:
         """
 
         data['date'] = pd.to_datetime(data['timestamp'], unit='ms')
         dates = self.__get_reference(_maximum=data['date'].max(), _minimum=data['date'].min())
-        frame = dates.merge(data, how='left', on='date')
+        logging.info(type(dates))
 
+        if dates.inferred_freq is None:
+            logging.info('Inferred Frequency of %s (%s): %s',
+                         partition.ts_id, partition.catchment_id, dates.inferred_freq)
+
+        frame = pd.DataFrame(data={'date': dates}).merge(data, how='left', on='date')
         logging.info('Shapes: %s, %s, %s', data.shape, dates.shape, frame.shape)
