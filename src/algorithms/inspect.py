c523a2205b1ec3175bb7fca63d252f128268e388
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-04-25 00:05:09 +0100

    Update src/algorithms/inspect.py

--- src/algorithms/inspect.py
+++ src/algorithms/inspect.py
@@ -1,28 +1,36 @@
+"""Module inspect.py"""
 import datetime
 import logging
 
 import pandas as pd
+
 import src.elements.partitions as pr
 
 
 class Inspect:
+    """
+    Ascertains date points; every 15 minutes.
+    """
 
     def __init__(self):
         pass
 
     @staticmethod
-    def __get_reference(_maximum, _minimum):
+    def __get_reference(_maximum: pd.Timestamp, _minimum: pd.Timestamp) -> pd.DatetimeIndex:
+        """
 
-        logging.info('_maximum: %s', type(_maximum))
+        :param _maximum:
+        :param _minimum:
+        :return:
+        """
 
         latest = datetime.datetime.now().year
-
         dates = pd.date_range(
             start=_minimum, end=_maximum, freq='0.25h', inclusive='left' if _maximum.year != latest else 'both')
 
         return dates
 
-    def exc(self, data: pd.DataFrame, partition: pr.Partitions):
+    def exc(self, data: pd.DataFrame, partition: pr.Partitions) -> pd.DataFrame:
         """
 
         :param data:
@@ -31,12 +39,12 @@ class Inspect:
         """
 
         data['date'] = pd.to_datetime(data['timestamp'], unit='ms')
-        dates = self.__get_reference(_maximum=data['date'].max(), _minimum=data['date'].min())
-        logging.info(type(dates))
+        dates: pd.DatetimeIndex = self.__get_reference(_maximum=data['date'].max(), _minimum=data['date'].min())
 
         if dates.inferred_freq is None:
             logging.info('Inferred Frequency of %s (%s): %s',
                          partition.ts_id, partition.catchment_id, dates.inferred_freq)
 
         frame = pd.DataFrame(data={'date': dates}).merge(data, how='left', on='date')
-        logging.info('Shapes: %s, %s, %s', data.shape, dates.shape, frame.shape)
+
+        return frame
