528b375df31d6978445f12a4247227e85361dc6a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-04-24 14:50:48 +0100

    Add src/main.py

---
+++ src/main.py
@@ -0,0 +1,25 @@
+"""Module main.py"""
+import datetime
+import logging
+import os
+import sys
+
+
+def main():
+
+    logger: logging.Logger = logging.getLogger(__name__)
+    logger.info('Starting: %s', datetime.datetime.now().isoformat(timespec='microseconds'))
+
+
+if __name__ == '__main__':
+
+    root = os.getcwd()
+    sys.path.append(root)
+    sys.path.append(os.path.join(root, 'src'))
+
+    # Logging
+    logging.basicConfig(level=logging.INFO,
+                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+    main()
