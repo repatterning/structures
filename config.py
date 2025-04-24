8dfcbe4d0d8621a8981433fbbf7b9938d5c9f678
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-04-24 18:29:36 +0100

    Add config.py

---
+++ config.py
@@ -0,0 +1,25 @@
+"""
+Module config
+"""
+import os
+import datetime
+import time
+
+
+class Config:
+    """
+    Class Config
+
+    For project settings
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+
+        self.warehouse: str = os.path.join(os.getcwd(), 'warehouse')
+        self.structures_ = os.path.join(self.warehouse, 'data', 'structures')
+
+        # Template
+        self.s3_parameters_key = 's3_parameters.yaml'
