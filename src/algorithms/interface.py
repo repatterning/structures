8b48998849f6ee02c16d7fcf3e32cdb58cdf85f6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-04-25 12:29:55 +0100

    Update src/algorithms/interface.py

--- src/algorithms/interface.py
+++ src/algorithms/interface.py
@@ -7,6 +7,7 @@ import config
 
 import src.algorithms.inspect
 import src.algorithms.resampling
+import src.algorithms.deduplicate
 import src.elements.partitions as pr
 import src.elements.text_attributes as txa
 import src.functions.streams
@@ -27,6 +28,7 @@ class Interface:
         self.__streams = src.functions.streams.Streams()
 
         self.__inspect = src.algorithms.inspect.Inspect()
+        self.__deduplicate = src.algorithms.deduplicate.Deduplicate()
         self.__resampling = src.algorithms.resampling.Resampling()
 
         self.__configurations = config.Config()
@@ -41,23 +43,6 @@ class Interface:
         text = txa.TextAttributes(uri=uri, header=0)
 
         return self.__streams.read(text=text)
-
-    @staticmethod
-    def __deduplicate(frame: pd.DataFrame) -> pd.DataFrame:
-        """
-
-        :param frame:
-        :return:
-        """
-
-        data = frame.sort_values(by=['timestamp', 'quality_code'], axis=0, ascending=True)
-        data.drop_duplicates(subset=['timestamp'], keep='first', inplace=True)
-
-        if data.shape[0] != frame.shape[0]:
-            logging.info('original, deduplicated, deduplicates: %s, %s\n%s',
-                         frame.shape, data.shape, data.head())
-
-        return data
 
     @staticmethod
     def __append_measure(frame: pd.DataFrame, gauge_datum: float):
@@ -100,7 +85,7 @@ class Interface:
         for partition in partitions[:3]:
 
             data = self.__get_data(uri=partition.uri)
-            data = self.__deduplicate(frame=data)
+            data = self.__deduplicate.exc(frame=data)
             data = self.__inspect.exc(frame=data.copy(), partition=partition)
 
             fundamentals = self.__append_measure(frame=data.copy(), gauge_datum=partition.gauge_datum)
