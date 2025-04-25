380afb5aab1816718c266e40a2b9104dd1285f07
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-04-25 20:46:41 +0100

    Update src/transfer/interface.py

--- src/transfer/interface.py
+++ src/transfer/interface.py
@@ -32,7 +32,7 @@ class Interface:
             bucket_name=self.__s3_parameters.configurations, key_name='references/metadata.json')
         self.__metadata = json.loads(buffer)
 
-    def __set_metadata(self, frame: pd.DataFrame):
+    def __set_metadata(self, frame: pd.DataFrame) -> pd.DataFrame:
         """
 
         :param frame:
@@ -41,6 +41,8 @@ class Interface:
 
         # Assign metadata dict strings via section values
         frame['metadata'] = frame['section'].map(lambda x: self.__metadata[str(x)])
+
+        return frame
 
 
     def exc(self):
@@ -52,15 +54,16 @@ class Interface:
         # The strings for transferring data to Amazon S3 (Simple Storage Service)
         strings: pd.DataFrame = src.transfer.dictionary.Dictionary().exc(
             path=os.path.join(os.getcwd(), 'warehouse'), extension='*', prefix='')
-        logging.info(strings)
 
         # Transfer
-        '''
+
         if strings.empty:
             logging.info('Empty')
         else:
+            strings = self.__set_metadata(frame=strings.copy())
+            logging.info(strings)
             messages = src.s3.ingress.Ingress(
                 service=self.__service, bucket_name=self.__s3_parameters.internal).exc(
                 strings=strings, tags={'project': 'hydrography'})
             logging.info(messages)
-        '''
+
