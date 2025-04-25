f446bbfe56a69d20aef045f98541a0524e05af1f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-04-25 01:51:17 +0100

    Update src/algorithms/interface.py

--- src/algorithms/interface.py
+++ src/algorithms/interface.py
@@ -1,22 +1,42 @@
 """Module interface.py"""
 import logging
+import os
 
 import pandas as pd
+import config
 
 import src.algorithms.inspect
+import src.algorithms.resampling
 import src.elements.partitions as pr
 import src.elements.text_attributes as txa
 import src.functions.streams
+import src.functions.directories
 
 
 class Interface:
+    """
+    Interface
+    """
 
     def __init__(self):
+        """
+        Constructor
+        """
 
+        self.__directories = src.functions.directories.Directories()
         self.__streams = src.functions.streams.Streams()
+
         self.__inspect = src.algorithms.inspect.Inspect()
+        self.__resampling = src.algorithms.resampling.Resampling()
+
+        self.__configurations = config.Config()
 
     def __get_data(self, uri: str) -> pd.DataFrame:
+        """
+
+        :param uri:
+        :return:
+        """
 
         text = txa.TextAttributes(uri=uri, header=0)
 
@@ -53,6 +73,23 @@ class Interface:
 
         return frame
 
+    def __persist(self, data: pd.DataFrame, endpoint: str, partition: pr.Partitions):
+        """
+
+        :param data:
+        :param endpoint:
+        :param partition:
+        :return:
+        """
+
+        path = os.path.join(endpoint, str(partition.catchment_id), str(partition.ts_id))
+        self.__directories.create(path=path)
+
+        message = self.__streams.write(
+            blob=data, path=os.path.join(path, f'{partition.datestr}.csv'))
+
+        return message
+
     def exc(self, partitions: list[pr.Partitions]):
         """
 
@@ -65,6 +102,11 @@ class Interface:
             data = self.__get_data(uri=partition.uri)
             data = self.__deduplicate(frame=data)
             data = self.__inspect.exc(frame=data.copy(), partition=partition)
-            data = self.__append_measure(frame=data.copy(), gauge_datum=partition.gauge_datum)
 
-            logging.info(data.head())
+            fundamentals = self.__append_measure(frame=data.copy(), gauge_datum=partition.gauge_datum)
+            self.__persist(data=fundamentals, endpoint=self.__configurations.fundamentals_, partition=partition)
+
+            resamples = self.__resampling.exc(data=fundamentals)
+            self.__persist(data=resamples, endpoint=self.__configurations.resamples_, partition=partition)
+
+            logging.info(resamples)
