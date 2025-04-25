6a5fd6cdd1ca31140c8ca3eb1c172a59999ce61f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-04-25 12:24:09 +0100

    Update src/algorithms/deduplicate.py

---
+++ src/algorithms/deduplicate.py
@@ -0,0 +1,35 @@
+"""Module deduplicate.py"""
+import logging
+
+import pandas as pd
+
+
+class Deduplicate:
+
+    def __init__(self):
+        """
+        Constructor
+        """
+
+        # Logging
+        logging.basicConfig(level=logging.INFO,
+                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d\n',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+
+        self.__logger = logging.getLogger(__name__)
+
+    def __deduplicate(self, frame: pd.DataFrame) -> pd.DataFrame:
+        """
+
+        :param frame:
+        :return:
+        """
+
+        data = frame.sort_values(by=['timestamp', 'quality_code'], axis=0, ascending=True)
+        data.drop_duplicates(subset=['timestamp'], keep='first', inplace=True)
+
+        if data.shape[0] != frame.shape[0]:
+            self.__logger.info('original, deduplicated, deduplicates: %s, %s\n%s',
+                         frame.shape, data.shape, data.head())
+
+        return data
