ba61188974fe5619aa76cc24fe7bc77cd0558880
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-04-26 02:46:00 +0100

    Update src/assets/partitions.py

--- src/assets/partitions.py
+++ src/assets/partitions.py
@@ -2,7 +2,6 @@
 import datetime
 import typing
 
-import dask
 import pandas as pd
 
 
@@ -24,22 +23,8 @@ class Partitions:
         # Fields
         self.__fields = ['ts_id', 'catchment_id', 'gauge_datum', 'datestr']
 
-    @dask.delayed
-    def __matrix(self, start: str):
-        """
-
-        :param start: The date string of the start date of a period; format YYYY-mm-dd.
-        :return:
-        """
-
-        data = self.__data.copy()
-
-        data = data.assign(datestr = str(start))
-        records: pd.DataFrame = data[self.__fields]
-
-        return records
-
-    def __boundaries(self) -> typing.Tuple[datetime.datetime, datetime.datetime]:
+    @staticmethod
+    def __boundaries() -> typing.Tuple[datetime.datetime, datetime.datetime]:
         """
         The boundaries of the dates; datetime format
 
@@ -51,13 +36,24 @@ class Partitions:
         ending = datetime.datetime.strptime(f'{_end}-01-01', '%Y-%m-%d')
 
         # Starting
-        if self.__attributes.get('reacquire'):
-            _start: int = datetime.datetime.strptime(self.__attributes.get('starting'), '%Y-%m-%d').year
-        else:
-            _start: int = _end - 1
+        _start: int = _end - 1
         starting = datetime.datetime.strptime(f'{_start}-01-01', '%Y-%m-%d')
 
         return starting, ending
+
+    def __dates(self, starting: datetime.datetime, ending: datetime.datetime) -> pd.DataFrame:
+        """
+
+        :param starting:
+        :param ending:
+        :return:
+        """
+
+        frame = pd.date_range(
+            start=starting, end=ending, freq=self.__attributes.get('frequency')
+        ).to_frame(index=False, name='datestr')
+
+        return frame['datestr'].apply(lambda x: x.strftime('%Y-%m-%d')).to_frame()
 
     def exc(self) -> pd.DataFrame:
         """
@@ -65,18 +61,11 @@ class Partitions:
         :return:
         """
 
+        if self.__attributes.get('reacquire'):
+            return self.__data
+
         starting, ending = self.__boundaries()
+        dates = self.__dates(starting=starting, ending=ending)
+        frame = dates.merge(self.__data, how='left', on='datestr')
 
-        # Create series
-        frame = pd.date_range(
-            start=starting, end=ending, freq=self.__attributes.get('frequency')).to_frame(index=False, name='date')
-        starts: pd.Series = frame['date'].apply(lambda x: x.strftime('%Y-%m-%d'))
-
-        # Compute partitions matrix
-        computations = []
-        for start in starts.values:
-            matrix = self.__matrix(start=start)
-            computations.append(matrix)
-        calculations = dask.compute(computations, scheduler='threads')[0]
-
-        return pd.concat(calculations, axis=0, ignore_index=True)
+        return frame
