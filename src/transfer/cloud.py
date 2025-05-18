d3162b467742c0e6acea4ed57b46a68f037b148e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-05-18 16:32:22 +0100

    Update src/transfer/cloud.py

--- src/transfer/cloud.py
+++ src/transfer/cloud.py
@@ -1,59 +1,86 @@
-"""Module cloud.py"""
-import logging
+"""
+Module setup.py
+"""
+import os
 
-import dask
-import numpy as np
-
-import src.elements.partitions as pr
+import config
 import src.elements.s3_parameters as s3p
-import src.s3.directives
+import src.elements.service as sr
+import src.functions.directories
+import src.s3.bucket
+import src.s3.prefix
 
 
 class Cloud:
     """
-    Preparing particular cloud spots
+
+    Notes
+    -----
+
+    This class prepares the Amazon S3 (Simple Storage Service) and local data environments.
     """
 
-    def __init__(self, s3_parameters: s3p.S3Parameters):
+    def __init__(self, service: sr.Service, s3_parameters: s3p.S3Parameters):
         """
 
+        :param service: A suite of services for interacting with Amazon Web Services.
         :param s3_parameters: The overarching S3 parameters settings of this project, e.g., region code
                               name, buckets, etc.
         """
 
-        self.__s3_parameters = s3_parameters
-        self.__directive = src.s3.directives.Directives()
+        self.__service: sr.Service = service
+        self.__s3_parameters: s3p.S3Parameters = s3_parameters
 
-    def __delete(self, section: str, partition: pr.Partitions) -> int:
+        # Configurations
+        self.__configurations = config.Config()
+
+        # An instance for interacting with objects within an Amazon S3 prefix
+        self.__pre = src.s3.prefix.Prefix(service=self.__service, bucket_name=self.__s3_parameters.internal)
+
+        self.__prefixes = [self.__s3_parameters.path_internal_data + os.path.basename(value)
+                           for value in [self.__configurations.resamples_, self.__configurations.fundamentals_] ]
+
+    def __clear_prefix(self) -> bool:
         """
 
-        :param section: fundamentals or resamples; the granularity of the former is 0.25 hours, whilst
-                        that of the latter is 1 hour.
-        :param partition: Refer to src/elements/partitions.py
         :return:
         """
 
-        state = self.__directive.delete(
-            path=f's3://{self.__s3_parameters.internal}/data/{section}/{partition.catchment_id}/{partition.ts_id}/')
+        # Get the keys therein
+        states = []
+        for prefix in self.__prefixes:
+            keys: list[str] = self.__pre.objects(prefix=prefix)
+            if len(keys) > 0:
+                objects = [{'Key' : key} for key in keys]
+                state = self.__pre.delete(objects=objects)
+                states.append(bool(state))
+            else:
+                states.append(True)
 
-        return state
+        return all(states)
 
-    def exc(self, partitions: list[pr.Partitions]):
+    def __s3(self, restart: bool) -> bool:
         """
+        Prepares an Amazon S3 (Simple Storage Service) bucket.
 
-        :param partitions: Refer to src/elements/partitions.py
+        :param restart: Is this part of a re-compute directive?
         :return:
         """
 
-        computations = []
-        for partition in partitions:
-            computations.append(
-                np.array([self.__delete(section='fundamentals', partition=partition),
-                          self.__delete(section='resamples', partition=partition)])
-            )
-        calculations = dask.compute(computations, scheduler='threads')[0]
+        # An instance for interacting with Amazon S3 buckets.
+        bucket = src.s3.bucket.Bucket(service=self.__service, location_constraint=self.__s3_parameters.location_constraint,
+                                      bucket_name=self.__s3_parameters.internal)
 
-        states = np.concat(calculations)
+        if bucket.exists():
+            return self.__clear_prefix() if restart else True
 
-        logging.info(states)
-        logging.info(sum(states))
+        return bucket.create()
+
+    def exc(self, restart: bool) -> bool:
+        """
+
+        :param restart: Is this part of a re-compute directive?
+        :return:
+        """
+
+        return self.__s3(restart=restart)
